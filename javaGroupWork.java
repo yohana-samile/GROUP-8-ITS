b8ed647d0ad17ff047fcc2fd6cf4d98954c41a38
yohana-samile <samileking9@gmail.com>
2022-06-08 02:14:47 +0300

    Revert Commit

---
+++ javaGroupWork.java
@@ -0,0 +1,21 @@
+public class javaGroupWork{
+    public static void main(String[] args){
+        String greeting, yohana, abubakari, shadrack, thank, regNo, regNo1, regNo2;
+        greeting = "\thello mr frank this is group 8 mambers.\n";
+
+        //for yohana
+        yohana = "Yohana samile";
+        regNo = "\t14322080/T.21\n";
+
+        //for pandu
+        regNo1 = "\tabubakari pandu\n";
+        abubakari = "14322047/T.21";
+
+        //for shadrack
+        shadrack = "shedrack musa";
+        regNo2 = "\t14322082/T.21\n";
+
+        thank = "\tThank you...\n";
+        System.out.printf(greeting.toUpperCase() + yohana + regNo + abubakari + regNo1 + shadrack + regNo2 + thank.toUpperCase());
+    }
+}
